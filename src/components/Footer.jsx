ed2ab704a6a4cf455ecfd2dc9762fd89a9ad6a1c
Suyash Jain <sj@Suyashs-MacBook-Air.local>
2024-12-02 23:18:02 +0530

    color updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Footer = () => {
-
     const today = new Date();
     const year = today.getFullYear();
 
@@ -9,7 +8,6 @@ const Footer = () => {
         <div className='w-screen text-xs sm:text-sm md:text-base lg:text-lg p-4 flex items-center justify-center absolute bottom-0 bg-black z-10'>
             <p className='text-center text-gray-400'>Copyright © {year} - All rights reserved</p>
         </div>
-
     )
 }
 
